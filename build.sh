092c19666ddcc230f36dd3ab5031c864b0281af5
Uwe Eisele <code@uweeisele.eu>
2023-06-10 16:57:51 +0200

    wip: use build script in github actions

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 BIN_NAME="${1:-godub}"
 
+echo CGO_ENABLED=0 go build -o "${BIN_NAME}" -ldflags "-s -w -X main.AppVersion=$(git describe --tags --dirty --abbrev=7) -X main.AppCommit=$(git rev-parse --short=7 HEAD)"
+
 CGO_ENABLED=0 go build -o "${BIN_NAME}" -ldflags "-s -w -X main.AppVersion=$(git describe --tags --dirty --abbrev=7) -X main.AppCommit=$(git rev-parse --short=7 HEAD)"
 upx -6 "${BIN_NAME}"
